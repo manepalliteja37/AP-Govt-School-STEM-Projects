a1b69df2c320a17eedf0184ef12665e80598cce9
Telugu Mad Thinker <manepalliteja37@gmail.com>
2026-03-02 11:26:08 +0530

    Update Smart-Lighting-LDR.ino

--- Smart-Lighting-LDR/Smart-Lighting-LDR.ino
+++ Smart-Lighting-LDR/Smart-Lighting-LDR.ino
@@ -1,24 +1,35 @@
-// LDR-based Smart Lighting Code
-#include <Arduino.h>
+#define LDR1 2
+#define LDR2 3
+#define LDR3 4
+#define LDR4 5
 
-// Pin definitions
-const int ldrPin = A0;  // LDR connected to A0
-const int ledPin = 9;   // LED connected to digital pin 9
+#define LED1 8
+#define LED2 9
+#define LED3 10
+#define LED4 11
 
 void setup() {
-    pinMode(ledPin, OUTPUT);  // Set LED pin as output
-    Serial.begin(9600);       // Start serial communication
+  pinMode(LDR1, INPUT);
+  pinMode(LDR2, INPUT);
+  pinMode(LDR3, INPUT);
+  pinMode(LDR4, INPUT);
+
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+}
+
+void controlLight(int ldrPin, int ledPin) {
+  if (digitalRead(ldrPin) == LOW)
+    digitalWrite(ledPin, HIGH);
+  else
+    digitalWrite(ledPin, LOW);
 }
 
 void loop() {
-    int ldrValue = analogRead(ldrPin); // Read the LDR value
-    Serial.println(ldrValue);          // Print the LDR value to serial monitor
-
-    // Control LED based on LDR value
-    if (ldrValue < 500) {   // Change the threshold value as needed
-        digitalWrite(ledPin, HIGH); // Turn ON the LED
-    } else {
-        digitalWrite(ledPin, LOW);  // Turn OFF the LED
-    }
-    delay(1000); // Wait for 1 second before the next read
+  controlLight(LDR1, LED1);
+  controlLight(LDR2, LED2);
+  controlLight(LDR3, LED3);
+  controlLight(LDR4, LED4);
 }
