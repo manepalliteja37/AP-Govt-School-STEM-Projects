659ee39b2d089f2fa5804af4f502eff28452725c
Telugu Mad Thinker <manepalliteja37@gmail.com>
2026-03-02 11:22:27 +0530

    Update Bluetooth-Home-Automation.ino

--- Bluetooth-Home-Automation/Bluetooth-Home-Automation.ino
+++ Bluetooth-Home-Automation/Bluetooth-Home-Automation.ino
@@ -1,22 +1,71 @@
-#include <BluetoothSerial.h>
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-BluetoothSerial SerialBT;
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+#define LED1 7
+#define LED2 8
+#define BT_STATE 2
+
+SoftwareSerial bluetooth(10, 11);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+bool led1State = false;
+bool led2State = false;
+bool btConnected = false;
+
+void updateDisplay() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+  display.setCursor(0, 0);
+  display.println(btConnected ? "Bluetooth: Connected" : "Bluetooth: Connecting");
+
+  display.setCursor(0, 20);
+  display.print("LED1: ");
+  display.println(led1State ? "ON" : "OFF");
+
+  display.setCursor(0, 35);
+  display.print("LED2: ");
+  display.println(led2State ? "ON" : "OFF");
+
+  display.display();
+}
 
 void setup() {
-  Serial.begin(115200);
-  SerialBT.begin("ESP32_Bluetooth"); // Name of your Bluetooth device
-  Serial.println("The device started, now you can pair it");
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(BT_STATE, INPUT);
+
+  bluetooth.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+  updateDisplay();
 }
 
 void loop() {
-  if (SerialBT.available()) {
-    char incoming = SerialBT.read();
-    if(incoming == '1') {
-      // Code to turn on a device
-      Serial.println("Device ON");
-    } else if(incoming == '0') {
-      // Code to turn off a device
-      Serial.println("Device OFF");
-    }
+
+  bool currentState = digitalRead(BT_STATE);
+  if (currentState != btConnected) {
+    btConnected = currentState;
+    updateDisplay();
+  }
+
+  if (bluetooth.available()) {
+    char cmd = bluetooth.read();
+
+    if (cmd == '1') led1State = true;
+    if (cmd == '2') led1State = false;
+    if (cmd == '3') led2State = true;
+    if (cmd == '4') led2State = false;
+
+    digitalWrite(LED1, led1State);
+    digitalWrite(LED2, led2State);
+
+    updateDisplay();
   }
 }
